f4598d5d2ae7094f6d366df4a81c2e82ec5be0a7
codersjj <shanewestlife@outlook.com>
2025-08-26 09:18:57 +0800

    feat: add a spotlight effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <main className="relative flex flex-col justify-center items-center mx-auto px-5 sm:px-10 overflow-hidden bg-black-100 text-white">
       <div className="max-w-7xl w-full">
-        <h1>Hello, portfolio!</h1>
+        <Hero />
       </div>
     </main>
   );
---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Spotlight } from './ui/spotlight-new'
+
+const Hero = () => {
+  return (
+    <div className='w-full h-screen pt-36 pb-20'>
+      <Spotlight />
+    </div>
+  )
+}
+
+export default Hero
