39d7c3d95464150da4c54e72f72395aa62e77c2e
codersjj <shanewestlife@outlook.com>
2025-08-28 10:47:32 +0800

    feat: create a custom ThemeProvider component for theme management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from "@/providers/theme-provider"
 import { MuteStoreProvider } from '@/providers/mute-store-provider';
 
 const geistSans = Geist({
@@ -29,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <MuteStoreProvider>
             {children}
           </MuteStoreProvider>
