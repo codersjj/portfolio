ca367bb7502fbb8cf8313e401dd7e26038685708
codersjj <shanewestlife@outlook.com>
2025-09-09 14:54:01 +0800

    feat: update work experience titles and descriptions

--- data/index.ts
+++ data/index.ts
@@ -103,22 +103,22 @@ export const projects = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Web Development Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using Vue.js, enhancing interactivity.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Web Development Engineer",
+    desc: "Designed and developed responsive web applications.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Frontend Developer",
+    desc: "Collaborated with designers to implement user-friendly web interfaces.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
