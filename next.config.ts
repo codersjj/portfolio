da7f7fa7532f8d1caedb99cacee8b14f76cc21de
codersjj <shanewestlife@outlook.com>
2025-09-11 10:57:31 +0800

    fix: update basePath logic for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.NODE_ENV === 'production' && !process.env.VERCEL_ENV
+
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: process.env.VERCEL_ENV ? '' : '/portfolio',
+  basePath: isGitHubPages ? '/portfolio' : '',
 };
 
 export default withSentryConfig(nextConfig, {
