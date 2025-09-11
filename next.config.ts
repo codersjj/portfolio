4da5d5fc9eec0be186bf1b9b6f700cf2f75aa086
codersjj <shanewestlife@outlook.com>
2025-09-11 10:29:33 +0800

    fix: update basePath configuration for Vercel environment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/portfolio',
+  basePath: process.env.VERCEL_ENV ? '' : '/portfolio',
 };
 
 export default withSentryConfig(nextConfig, {
