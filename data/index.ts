5e755fb36f14be56ba36cae76f5cedade9b92b9f
codersjj <shanewestlife@outlook.com>
2025-11-27 17:38:32 +0800

    chore: update the inside scoop

--- data/index.ts
+++ data/index.ts
@@ -53,7 +53,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building Wave AI — a full-stack second-brain AI agent",
+    title: "Currently building VC Interview — a full-stack MERN video-interview & coding-practice platform",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
